274eaed3551eb56d833cd187c147c4a2db492214
Corbin Stark <starkcorbin@gmail.com>
2018-03-01 21:24:32 -0800

    Added license information to files
    Also changed #pragma once to standard headers

--- Source Code/colors.h
+++ Source Code/colors.h
@@ -1,7 +1,35 @@
-#pragma once
+///////////////////////////////////////////////////////////////////////////
+// FILE:			           colors.h                                  //
+///////////////////////////////////////////////////////////////////////////
+//                      BAHAMUT GRAPHICS LIBRARY                         //
+//						  Author: Corbin Stark							 //
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+// Permission is hereby granted, free of charge, to any person obtaining //
+// a copy of this software and associated documentation files (the       //
+// "Software"), to deal in the Software without restriction, including   //
+// without limitation the rights to use, copy, modify, merge, publish,   //
+// distribute, sublicense, and/or sell copies of the Software, and to    //
+// permit persons to whom the Software is furnished to do so, subject to //
+// the following conditions:                                             //
+//                                                                       //
+// The above copyright notice and this permission notice shall be        //
+// included in all copies or substantial portions of the Software.       //
+//                                                                       //
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       //
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    //
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.//
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  //
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  //
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     //
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                //
+///////////////////////////////////////////////////////////////////////////
+
+#ifndef COLORS_H
+#define COLORS_H
+
 #include "vec4f.h"
 
-//stealing the colors from raylib for now. I like these colors.
 static vec4f LIGHTGRAY = vec4f(200, 200, 200, 255);
 static vec4f GRAY = vec4f(130, 130, 130, 255);
 static vec4f DARKGRAY = vec4f(80, 80, 80, 255);
@@ -28,5 +56,5 @@ static vec4f WHITE = vec4f(255, 255, 255, 255);
 static vec4f BLACK = vec4f(0, 0, 0, 255);
 static vec4f BLANK = vec4f(0, 0, 0, 0);
 static vec4f MAGENTA = vec4f(255, 0, 255, 255);
-static vec4f RAYWHITE = vec4f(245, 245, 245, 255);
-//stealing the colors from raylib for now. I like these colors.
+
+#endif
